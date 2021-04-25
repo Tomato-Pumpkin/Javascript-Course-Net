08c70d44ad4d89ca207e62b460f937aaefacedc2
Tomato-Pumpkin <yogi.firdiansyah.yogi@gmail.com>
2021-04-25 11:21:31 -0700

    Tugas Kondisi

---
+++ kondisi.js
@@ -0,0 +1,36 @@
+// var a = 5;
+// if (a == 5){
+//     console.log(a +" adalah ganjil");
+// }else {
+//     console.log(a + " adalah genap");
+// }
+
+// console.log(a);
+
+
+var score = 87;
+var grade;
+if (score <= 100 && score >85){
+    grade = "Student mendapat nilai 'A' ";
+} else if (score <= 84 && score > 70 ){
+    grade = "Student mendapat nilai 'B' ";
+} else if (score <= 69 && score > 50){
+    grade = "Student mendapat nilai 'C' ";
+} else if (score <= 49 && score > 30){
+    grade = "Student mendapat nilai 'D' ";
+} else if (score <= 29 && score > 0) {
+    grade = "Student mendapat nilai 'E' ";
+}
+console.log(grade);
+
+
+// var age = 13;
+// var uang = 50000;
+
+// if (age<15){
+// console.log("Tidak boleh masuk");
+// } if(uang<50000){
+// console.log("Uang tidak cukup");
+// } else {
+//  console.log("Boleh masuk");
+// }
