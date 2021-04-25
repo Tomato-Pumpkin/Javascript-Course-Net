5a62441703b3aa5ddd5f74a0f8e90c12a05889b8
Tomato-Pumpkin <yogi.firdiansyah.yogi@gmail.com>
2021-04-25 11:52:26 -0700

    Ada perubahan di soal nomor 3

--- kondisi.js
+++ kondisi.js
@@ -7,30 +7,31 @@
 
 // console.log(a);
 
-
-var score = 87;
-var grade;
-if (score <= 100 && score >85){
-    grade = "Student mendapat nilai 'A' ";
-} else if (score <= 84 && score > 70 ){
-    grade = "Student mendapat nilai 'B' ";
-} else if (score <= 69 && score > 50){
-    grade = "Student mendapat nilai 'C' ";
-} else if (score <= 49 && score > 30){
-    grade = "Student mendapat nilai 'D' ";
-} else if (score <= 29 && score > 0) {
-    grade = "Student mendapat nilai 'E' ";
-}
-console.log(grade);
-
-
-// var age = 13;
-// var uang = 50000;
-
-// if (age<15){
-// console.log("Tidak boleh masuk");
-// } if(uang<50000){
-// console.log("Uang tidak cukup");
-// } else {
-//  console.log("Boleh masuk");
+// var score = 55;
+// var grade;
+// if (score <= 100 && score >85){
+//     grade = "Student mendapat nilai 'A' ";
+// } else if (score <= 84 && score > 70 ){
+//     grade = "Student mendapat nilai 'B' ";
+// } else if (score <= 69 && score > 50){
+//     grade = "Student mendapat nilai 'C' ";
+// } else if (score <= 49 && score > 30){
+//     grade = "Student mendapat nilai 'D' ";
+// } else if (score <= 29 && score > 0) {
+//     grade = "Student mendapat nilai 'E' ";
 // }
+// console.log(grade);
+
+var umur = 18;
+var uang = 40000;
+
+if (umur >= 15) {
+  if (uang <= 50000) {
+    console.log("Uang tidak cukup");
+  } else {
+    console.log("Boleh masuk");
+  }
+} else {
+  console.log("Tidak boleh masuk");
+}
+
