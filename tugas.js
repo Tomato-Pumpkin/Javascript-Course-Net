ff4f667d5b85499228967d9ee095749b44cc4784
Tomato-Pumpkin <yogi.firdiansyah.yogi@gmail.com>
2021-04-25 09:22:53 -0700

    Mengumpulkan tugas Algoritma

---
+++ tugas.js
@@ -0,0 +1,164 @@
+/* I.
+    How to make indomie
+    1. choose your favourite indomie
+    2. prepare the tools (scissor, pan, plate, spoon)
+    3. put it pan on gas stove
+    4. input 7000ml water on pan
+    5. turn on the gas stove
+    6. wait until water on pan boiled
+    7. open the indomie with scissor
+    8. put it indomie to boiled water on pan
+    9. take the seasoning, sauce, and oil
+    10. open the seasoning, sauce, and oil
+    11. put it plate the seasoning, sauce, and oil
+    12. after indomie cooked, remove and drain on plate with seasoning, sauce, and oil
+    13. mix it
+    14. indomie ready to serve
+    15. enjoy your meal
+*/
+
+/*  II.
+    how to make sirloin steak
+    0. prepare the tools (pan, plate)
+    1. prepare 200gr Sirloin beef
+    2. 2tablespoon of oyster souce
+    3. 1/2teaspoon of ground pepper
+    4. 1/2teaspoon of salt
+    5. prepare margarine
+    6. Wash the meat thoroughly
+    7. stick it with a fork
+    8. add the oyster sauce
+    9. add epper and salt
+    10. coat until evenly distributed and let stand for about 1 hour so that the spices are absorbed
+    11. melt the margarine on pan
+    12. put the sirloin beef into pan
+    13. cook according with taste (rare, medium rare, medium, medium well, well done)
+    14. put the steak, serve it hot
+    15. enjoy your meal
+*/
+
+/* III.
+    algoritma dari luas persegi panjang
+    1. tentukan rumus luas persegi panjang
+    2. rumus persegi panjang adalah luas = panjang * lebar
+    3. buat variabel panjang
+    4. buat variabel lebar
+    5. buat variabel luas
+    6. masukan rumus luas persegi panjang ke dalam variabel luas
+    7. tampilkan luas persegi panjang
+
+    pseudocode dari luas persegi panjang
+    1. store panjang with any number
+    2. store lebar with any number
+    3. store luas with 
+    4. store luas with panjang times lebar
+    5. display luas
+*/
+
+/* IV.
+    algoritma dari volume bola
+    1. tentukan rumus volume bola
+    2. rumus bola adalah 4/3 pi r * r * r
+    3. buat variabel pi
+    4. buat variabel r
+    5. buat variabel volume
+    6. masukan rumus volume ke dalam variable volume
+    7. tampilkan volume
+
+    pseudocode dari luas persegi panjang
+    1. store pi
+    2. store r with any number
+    3. store volume
+    4. store volume with 4/3 times p1 times r times r times r
+    5. display volume
+*/
+
+/* V.
+    Algoritma Add, commit, push to github
+    1. Open Github
+    2. Click Code
+    3. Copy Link Repository
+    4. Git clone paste link repository
+    5. Git status : Check status changes on repository
+    6. Git add .
+    7. Git status : Check status changes on repository
+    8. Git commit -m "Make a Message to describing file"
+    9. Git push origin branch name
+    10. Git status : Check status changes on repository
+*/
+
+/* VI
+    algoritma dari keliling persegi
+    1. tentukan rumus keliling persegi
+    2. rumus keliling persegi adalah keliling = panjang + lebar + panjang + lebar
+    3. buat variabel panjang
+    4. buat variabel lebar
+    5. buat variabel keliling
+    6. masukan rumus keliling persegi ke dalam variabel keliling
+    7. tampilkan keliling persegi
+
+    pseudocode
+    1. store panjang with any number
+    2. store lebar with any number
+    3. store keliling
+    4. store keliling with panjang plus lebar plus panjang plus lebar
+    5. display keliling
+*/
+    // var panjang = 5;
+    // var lebar = 10;
+    // var keliling;
+    
+    // keliling = panjang + lebar + panjang + lebar;
+    // console.log(keliling);
+
+    /*
+    algoritma dari luas permukaan balok
+    1. tentukan rumus luas permukaan balok
+    2. rumus permukaan balok adalah adalah L = 2 * ( (p * l) + (p * t) + (l * t) )
+    3. buat variabel p
+    4. buat variabel l
+    5. buat variabel t
+    6. buat variabel L
+    7. masukan rumus luas permukaan balok ke dalam variabel L
+    8. tampilkan luas permukaan balok
+
+    pseudocode
+    1. store p with any number
+    2. store l with any number
+    3. store t with any number
+    4. store L
+    4. store L with 2 times ((p times l) plus (p times t) plus (l times t))
+    5. display L
+*/
+// var p = 5;
+// var l = 10;
+// var t = 5;
+// var L;
+
+// L = 2*( (p * l) + (p * t) + (l * t) );
+// console.log(L);
+
+/* VII
+    Who are You...?
+*/
+    //  var name = "Yogi";
+    //  var age = "30";
+    //  var Who;
+
+    //  Who= "My Name is " + name + ", and i'm " + age + " Years old";
+    // console.log(Who);
+
+/* VIII
+    Random Number 1 - 10
+*/
+    //  var number = Math.floor(Math.random() * 10) + 1;
+    //  console.log(number);
+/* IX
+    Membuat nomor induk mahasiswa
+*/
+    var nama = "Yogi";
+    var nomor = Math.floor(Math.random() * 50) + 1;
+    var stat = true;
+    var hasil;
+    hasil = nama + nomor + stat;
+    console.log(hasil);
